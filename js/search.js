22b83564322f69d51e0c4ae8e54f5b624e60bbf4
MichelleCody <michelle.cody18@gmail.com>
2016-04-25 15:18:22 -0400

    webpage changes

---
+++ js/search.js
@@ -0,0 +1,13 @@
+//We are SO sad
+
+
+// function setUp(){
+
+// document.getElementById("searchSubmit").onclick = function(){
+// 	location.href="./searchResults.html";
+// 	var div = document.createElement("div");
+// 	div.className = "huge";
+// 	div.innerText = "LOOK AT ME";
+// 	document.appendChild(div);
+// };
+// }
